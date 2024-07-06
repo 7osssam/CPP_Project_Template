04b1ba0d6424697f177f987d6edd392224975725
Hossam <7osssam@protonmail.ch>
2024-07-06 08:01:17 +0300

    Add Bank and BankAccount modules to the project

---
+++ src/BankAccount/BankAccount.hpp
@@ -0,0 +1,28 @@
+#ifndef BANKACCOUNT_HPP
+#define BANKACCOUNT_HPP
+
+#include <string>
+#include <iostream>
+class BankAccount
+{
+private:
+	int			accountNumber;
+	std::string accountHolder;
+	std::string accountType;
+	double		accountBalance{};
+
+public:
+	// Default constructor
+	BankAccount(int accountNumber, const std::string& accountHolder, const std::string& accountType,
+				double accountBalance);
+
+	void			   deposit(double amount);
+	bool			   withdraw(double amount);
+	int				   getAccountNumber() const;
+	const std::string& getAccountHolder() const;
+	const std::string& getAccountType() const;
+	double			   getAccountBalance() const;
+	void			   displayAccountInfo() const;
+};
+
+#endif // BANKACCOUNT_HPP
